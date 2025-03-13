ea6522bab998d0d1667dc30d0cfa8d2afb7d1c9a
bondarika <bondarika@yandex.ru>
2025-03-13 22:03:05 +0300

    configure backend

---
+++ server/index.js
@@ -0,0 +1,22 @@
+﻿require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const pool = require("./db");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+const start = async () => {
+  try {
+    pool
+      .connect()
+      .then(() => console.log("Connected to DB"))
+      .catch((err) => console.log(`Error connecting to DB: ${err}`));
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
