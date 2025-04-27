83d493e2fe202c398f8133652df05e11a5c8f90a
bondarika <bondarika@yandex.ru>
2025-04-27 18:42:48 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -9,19 +9,19 @@ const errorMiddleware = require('./middleware/error-middleware');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'verification'],
-  credentials: true
-};
-
 // const corsOptions = {
-//   origin: 'https://lms.stekpro.ru',
+//   origin: 'http://localhost:5173',
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //   allowedHeaders: ['Content-Type', 'Authorization', 'verification'],
-//   credentials: true,
+//   credentials: true
 // };
+
+const corsOptions = {
+  origin: 'https://lms.stekpro.ru',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'verification'],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
