c632544997cf0993b0e47f98122d1910d9047871
bondarika <bondarika@yandex.ru>
2025-04-28 03:58:15 +0300

    fix

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,8 +1,8 @@
 ﻿import axios from 'axios';
 import { AuthResponse } from '../types/response/AuthResponse';
 
-export const API_URL = 'http://localhost:5000/api';
-// export const API_URL = 'https://lms.stekpro.ru/api';
+// export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'https://lms.stekpro.ru/api';
 
 const api = axios.create({
   withCredentials: true,
